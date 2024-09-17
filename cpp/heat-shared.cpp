68df7f6a64f777f384089fad174d0e0807e79691
Patrick Diehl <diehlpk@lanl.gov>
2024-09-16 20:39:12 -0600

    Add openmp code

--- cpp/heat-shared.cpp
+++ cpp/heat-shared.cpp
@@ -1,79 +1,51 @@
 #include <iostream>
 #include <cmath>
-#include <mpi.h>
+#include <omp.h>
 
-// Function to compute the right-hand side of the heat equation
-void heatEquation(double *u, double *du_dt, double dx, double alpha, int nx) {
-  for (int i = 1; i < nx-1; i++) {
-    du_dt[i] = alpha * (u[i+1] - 2*u[i] + u[i-1]) / dx / dx;
-  }
-}
+#define NX 100  // Number of grid points in x-direction
+#define NT 100  // Number of time steps
+#define LX 1.0  // Length of the domain in x-direction
+#define T_FINAL 1.0  // Final time
+#define ALPHA 0.1  // Thermal diffusivity
+#define DX (LX / (NX - 1))  // Grid spacing in x-direction
+#define DT (T_FINAL / NT)  // Time step
 
-// Function to perform one time step using the Euler method
-void eulerStep(double *u, double *du_dt, double dt, int nx) {
-  for (int i = 0; i < nx; i++) {
-    u[i] += dt * du_dt[i];
-  }
-}
+void heat_equation_1d_parallel() {
+  int i, j;
+  double *u, *u_new;
+  u = new double[NX];
+  u_new = new double[NX];
 
-int main() {
-  // Initialize MPI
-  MPI_Init(NULL, NULL);
-
-  // Get the number of processes
-  int num_procs;
-  MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
-
-  // Get the rank of this process
-  int rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-  // Set up the problem parameters
-  int nx = 100;
-  double L = 1.0;
-  double dx = L / (nx - 1);
-  double t_end = 0.1;
-  double alpha = 0.1;
-  double dt = t_end / 100;
-
-  // Set up the initial condition
-  double *u = new double[nx];
-  for (int i = 0; i < nx; i++) {
-    u[i] = sin(M_PI * i * dx);
+  // Initialize the grid
+  for (i = 0; i < NX; i++) {
+    u[i] = sin(M_PI * i * DX);  // Initial condition
   }
 
-  // Set up the MPI data types
-  MPI_Datatype mpi_double;
-  MPI_Type_contiguous(nx, MPI_DOUBLE, &mpi_double);
-  MPI_Type_commit(&mpi_double);
+  // Time-stepping loop
+  for (j = 0; j < NT; j++) {
+    #pragma omp parallel for private(i)
+    for (i = 1; i < NX - 1; i++) {
+      u_new[i] = u[i] + ALPHA * DT / DX / DX * (u[i + 1] - 2 * u[i] + u[i - 1]);
+    }
 
-  // Solve the heat equation in parallel
-  for (int i = 0; i < 100; i++) {
-    double *du_dt = new double[nx];
-    heatEquation(u, du_dt, dx, alpha, nx);
-    eulerStep(u, du_dt, dt, nx);
-    delete[] du_dt;
-  }
-
-  // Validate the solution
-  double *u_exact = new double[nx];
-  for (int i = 0; i < nx; i++) {
-    u_exact[i] = sin(M_PI * i * dx) * exp(-alpha * M_PI * M_PI * t_end);
-  }
-
-  // Print the results
-  if (rank == 0) {
-    for (int i = 0; i < nx; i++) {
-      std::cout << "u[" << i << "] = " << u[i] << ", u_exact[" << i << "] = " << u_exact[i] << std::endl;
+    // Update the grid
+    #pragma omp parallel for private(i)
+    for (i = 1; i < NX - 1; i++) {
+      u[i] = u_new[i];
     }
   }
 
-  // Clean up
-  delete[] u;
-  delete[] u_exact;
-  MPI_Type_free(&mpi_double);
-  MPI_Finalize();
+  // Output the final solution
+  for (i = 0; i < NX; i++) {
+    std::cout << "u[" << i << "] = " << u[i] << std::endl;
+  }
 
+  delete[] u;
+  delete[] u_new;
+}
+
+int main() {
+  heat_equation_1d_parallel();
   return 0;
 }
 
--- fortran/heat-shared.f90
+++ fortran/heat-shared.f90
@@ -1,87 +1,38 @@
-PROGRAM heat_equation
-  USE MPI
-
+PROGRAM heat_equation_1d_parallel
+  USE omp_lib
   IMPLICIT NONE
 
-  INTEGER, PARAMETER :: nx = 100
-  INTEGER, PARAMETER :: nt = 100
-  REAL, PARAMETER :: L = 1.0
-  REAL, PARAMETER :: alpha = 0.1
-  REAL, PARAMETER :: t_end = 0.1
+  INTEGER, PARAMETER :: NX = 100  ! Number of grid points in x-direction
+  INTEGER, PARAMETER :: NT = 100  ! Number of time steps
+  REAL, PARAMETER :: LX = 1.0  ! Length of the domain in x-direction
+  REAL, PARAMETER :: T_FINAL = 1.0  ! Final time
+  REAL, PARAMETER :: ALPHA = 0.1  ! Thermal diffusivity
+  REAL, PARAMETER :: DX = LX / (NX - 1)  ! Grid spacing in x-direction
+  REAL, PARAMETER :: DT = T_FINAL / NT  ! Time step
 
-  INTEGER :: i, j
-  REAL :: dx, dt
-  REAL, DIMENSION(nx) :: u, du_dt
-  REAL, DIMENSION(nx) :: u_exact
+  REAL, DIMENSION(NX) :: u, u_new
 
-  INTEGER :: ierr
-  INTEGER :: num_procs, rank
-
-  CALL MPI_INIT(ierr)
-  CALL MPI_COMM_SIZE(MPI_COMM_WORLD, num_procs, ierr)
-  CALL MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierr)
-
-  dx = L / (nx - 1)
-  dt = t_end / nt
-
-  IF (rank == 0) THEN
-    DO i = 1, nx
-      u(i) = SIN(M_PI * i * dx)
-    END DO
-  END IF
-
-  CALL MPI_BCAST(u, nx, MPI_REAL, 0, MPI_COMM_WORLD, ierr)
-
-  DO j = 1, nt
-    CALL heat_equation_mpi(u, du_dt, dx, alpha, nx)
-    CALL euler_step_mpi(u, du_dt, dt, nx)
+  ! Initialize the grid
+  DO i = 1, NX
+    u(i) = SIN(M_PI * (i - 1) * DX)  ! Initial condition
   END DO
 
-  IF (rank == 0) THEN
-    DO i = 1, nx
-      u_exact(i) = SIN(M_PI * i * dx) * EXP(-alpha * M_PI * M_PI * t_end)
+  ! Time-stepping loop
+  DO j = 1, NT
+    !$OMP PARALLEL DO PRIVATE(i)
+    DO i = 2, NX - 1
+      u_new(i) = u(i) + ALPHA * DT / DX / DX * (u(i + 1) - 2 * u(i) + u(i - 1))
     END DO
+    !$OMP END PARALLEL DO
 
-    WRITE (*,*) 'Numerical solution:'
-    DO i = 1, nx
-      WRITE (*,*) 'u(', i, ') = ', u(i)
-    END DO
+    ! Update the grid
+    u = u_new
+  END DO
 
-    WRITE (*,*) 'Exact solution:'
-    DO i = 1, nx
-      WRITE (*,*) 'u_exact(', i, ') = ', u_exact(i)
-    END DO
-  END IF
+  ! Output the final solution
+  DO i = 1, NX
+    WRITE (*,*) 'u(', i, ') = ', u(i)
+  END DO
 
-  CALL MPI_FINALIZE(ierr)
-
-CONTAINS
-
-  SUBROUTINE heat_equation_mpi(u, du_dt, dx, alpha, nx)
-    REAL, INTENT(IN) :: u(nx)
-    REAL, INTENT(OUT) :: du_dt(nx)
-    REAL, INTENT(IN) :: dx, alpha
-    INTEGER, INTENT(IN) :: nx
-
-    INTEGER :: i
-
-    DO i = 2, nx-1
-      du_dt(i) = alpha * (u(i+1) - 2*u(i) + u(i-1)) / dx / dx
-    END DO
-  END SUBROUTINE heat_equation_mpi
-
-  SUBROUTINE euler_step_mpi(u, du_dt, dt, nx)
-    REAL, INTENT(INOUT) :: u(nx)
-    REAL, INTENT(IN) :: du_dt(nx)
-    REAL, INTENT(IN) :: dt
-    INTEGER, INTENT(IN) :: nx
-
-    INTEGER :: i
-
-    DO i = 1, nx
-      u(i) = u(i) + dt * du_dt(i)
-    END DO
-  END SUBROUTINE euler_step_mpi
-
-END PROGRAM heat_equation
+END PROGRAM heat_equation_1d_parallel
 
