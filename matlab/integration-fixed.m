8e4e0d61ba487ab401f9ffebac51010aae887aa5
NoujoudNader <101670658+NoujoudNader@users.noreply.github.com>
2024-12-06 15:43:44 -0600

    Create integration-fixed.m

---
+++ matlab/integration-fixed.m
@@ -0,0 +1,9 @@
+% Define the limits of integration
+a = -pi;
+b = 2/3 * pi;
+
+% Compute the area using the definite integral
+area = integral(sin(x), a, b);
+
+% Print the result
+fprintf('Area: %f\n', area);
